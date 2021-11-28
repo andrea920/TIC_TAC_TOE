ef0b416f95ffa6a9bc971609ce62ce3b793ba23e
Marina de Robles <marina.derobles@outlook.ph>
2021-11-29 00:48:17 +0700

    All working

---
+++ TIC_TAC_TOE/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace TIC_TAC_TOE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExitGame = new System.Windows.Forms.Button();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.lblDraw = new System.Windows.Forms.Label();
+            this.panelDisplay = new System.Windows.Forms.Panel();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.panelDisplay.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn00.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn00.Location = new System.Drawing.Point(21, 124);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(130, 130);
+            this.btn00.TabIndex = 0;
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn01
+            // 
+            this.btn01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn01.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn01.Location = new System.Drawing.Point(157, 124);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(130, 130);
+            this.btn01.TabIndex = 0;
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn02
+            // 
+            this.btn02.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn02.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn02.Location = new System.Drawing.Point(293, 124);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(130, 130);
+            this.btn02.TabIndex = 0;
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn10
+            // 
+            this.btn10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn10.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10.Location = new System.Drawing.Point(21, 260);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(130, 130);
+            this.btn10.TabIndex = 0;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn11
+            // 
+            this.btn11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn11.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(157, 260);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(130, 130);
+            this.btn11.TabIndex = 0;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn12
+            // 
+            this.btn12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn12.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(293, 260);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(130, 130);
+            this.btn12.TabIndex = 0;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn20
+            // 
+            this.btn20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn20.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn20.Location = new System.Drawing.Point(21, 396);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(130, 130);
+            this.btn20.TabIndex = 0;
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn21
+            // 
+            this.btn21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn21.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn21.Location = new System.Drawing.Point(157, 396);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(130, 130);
+            this.btn21.TabIndex = 0;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btn22
+            // 
+            this.btn22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn22.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn22.Location = new System.Drawing.Point(293, 396);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(130, 130);
+            this.btn22.TabIndex = 0;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DimGray;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(470, 145);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(143, 51);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.DimGray;
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(470, 203);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(143, 51);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExitGame
+            // 
+            this.btnExitGame.BackColor = System.Drawing.Color.DimGray;
+            this.btnExitGame.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExitGame.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExitGame.ForeColor = System.Drawing.Color.White;
+            this.btnExitGame.Location = new System.Drawing.Point(470, 475);
+            this.btnExitGame.Name = "btnExitGame";
+            this.btnExitGame.Size = new System.Drawing.Size(143, 51);
+            this.btnExitGame.TabIndex = 1;
+            this.btnExitGame.Text = "Exit Game";
+            this.btnExitGame.UseVisualStyleBackColor = false;
+            this.btnExitGame.Click += new System.EventHandler(this.btnExitGame_Click);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblX.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.ForeColor = System.Drawing.Color.White;
+            this.lblX.Location = new System.Drawing.Point(457, 293);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(72, 23);
+            this.lblX.TabIndex = 2;
+            this.lblX.Text = "label1";
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblO.ForeColor = System.Drawing.Color.White;
+            this.lblO.Location = new System.Drawing.Point(457, 335);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(72, 23);
+            this.lblO.TabIndex = 2;
+            this.lblO.Text = "label1";
+            // 
+            // lblDraw
+            // 
+            this.lblDraw.AutoSize = true;
+            this.lblDraw.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDraw.ForeColor = System.Drawing.Color.White;
+            this.lblDraw.Location = new System.Drawing.Point(457, 373);
+            this.lblDraw.Name = "lblDraw";
+            this.lblDraw.Size = new System.Drawing.Size(72, 23);
+            this.lblDraw.TabIndex = 2;
+            this.lblDraw.Text = "label1";
+            // 
+            // panelDisplay
+            // 
+            this.panelDisplay.BackColor = System.Drawing.Color.DimGray;
+            this.panelDisplay.Controls.Add(this.lblDisplay);
+            this.panelDisplay.Location = new System.Drawing.Point(21, 43);
+            this.panelDisplay.Name = "panelDisplay";
+            this.panelDisplay.Size = new System.Drawing.Size(402, 69);
+            this.panelDisplay.TabIndex = 3;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.ForeColor = System.Drawing.Color.White;
+            this.lblDisplay.Location = new System.Drawing.Point(91, 13);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(205, 40);
+            this.lblDisplay.TabIndex = 0;
+            this.lblDisplay.Text = "Game Start";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(635, 550);
+            this.Controls.Add(this.panelDisplay);
+            this.Controls.Add(this.lblDraw);
+            this.Controls.Add(this.lblO);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.btnExitGame);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "TIC TAC TOE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelDisplay.ResumeLayout(false);
+            this.panelDisplay.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExitGame;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblO;
+        private System.Windows.Forms.Label lblDraw;
+        private System.Windows.Forms.Panel panelDisplay;
+        private System.Windows.Forms.Label lblDisplay;
+    }
+}
+
---
+++ TIC_TAC_TOE/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIC_TAC_TOE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int player = 2;
+        public int turns = 0;
+        public int X = 0;
+        public int O = 0;
+        public int D = 0;
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "")
+            {
+                if (player % 2 == 0)
+                {
+                    btn.Text = "X";
+                    btn.ForeColor = Color.Blue;
+                    lblDisplay.Text = "O Turn Now";
+                    player++;
+                    turns++;
+                }
+                else
+                {
+                    btn.Text = "0";
+                    btn.ForeColor = Color.Red;
+                    lblDisplay.Text = "X Turn Now";
+                    player++;
+                    turns++;
+                }
+                if (Draw() == true)
+                {
+                    lblDisplay.Text = "Draw Game";
+                    MessageBox.Show("Game Draw, Click OK");
+                    lblDisplay.Text = "Game Start";
+                    D++;
+                    ClearBoard();
+                }
+                if (GameWinner() == true)
+                {
+                    if (btn.Text == "X")
+                    {
+                        lblDisplay.Text = "X Won";
+                        MessageBox.Show("X Won, Click OK");
+                        lblDisplay.Text = "Game Start";
+                        X++;
+                        ClearBoard();
+                    }
+                    else
+                    {
+                        lblDisplay.Text = "O Won";
+                        MessageBox.Show("O Won, Click OK");
+                        lblDisplay.Text = "Game Start";
+                        O++;
+                        ClearBoard();
+                    }
+                }
+            }
+        }
+
+        private void btnExitGame_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblX.Text = "X: " + X;
+            lblO.Text = "O: " + O;
+            lblDraw.Text = "Draw: " + D;
+        }
+        void ClearBoard()
+        {
+            player = 2;
+            turns = 0;
+            btn00.Text = btn01.Text = btn02.Text = btn10.Text = btn11.Text = btn12.Text = btn20.Text = btn21.Text = btn22.Text = "";
+            lblX.Text = "X: " + X;
+            lblO.Text = "O: " + O;
+            lblDraw.Text = "Draw: " + D;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearBoard();
+        }
+        bool Draw()
+        {
+            if ((turns == 9) && GameWinner() == false)
+                return true;
+            else
+                return false;
+        }
+        bool GameWinner()
+        {
+            //horizontal
+            if ((btn00.Text == btn01.Text) && (btn01.Text == btn02.Text) && btn00.Text != "")
+                return true;
+            else if ((btn10.Text == btn11.Text) && (btn11.Text == btn12.Text) && btn10.Text != "")
+                return true;
+            else if ((btn20.Text == btn21.Text) && (btn21.Text == btn22.Text) && btn20.Text != "")
+                return true;
+
+            //vertical
+            if ((btn00.Text == btn10.Text) && (btn10.Text == btn20.Text) && btn00.Text != "")
+                return true;
+            else if ((btn01.Text == btn11.Text) && (btn11.Text == btn21.Text) && btn01.Text != "")
+                return true;
+            else if ((btn02.Text == btn12.Text) && (btn12.Text == btn22.Text) && btn02.Text != "")
+                return true;
+
+            //diagonal
+            if ((btn00.Text == btn11.Text) && (btn11.Text == btn22.Text) && btn00.Text != "")
+                return true;
+            else if ((btn02.Text == btn11.Text) && (btn11.Text == btn20.Text) && btn02.Text != "")
+                return true;
+            else return false;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            X = O = D = 0;
+            ClearBoard();
+        }
+    }
+}
